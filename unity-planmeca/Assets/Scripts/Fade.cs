c3b407f637f9e558695b9163a5150cf44675ad52
Ilyas Hamadouche <ilyas.match@gmail.com>
2017-11-26 00:22:31 +0200

    commenting Fade.cs

--- unity-planmeca/Assets/Scripts/Fade.cs
+++ unity-planmeca/Assets/Scripts/Fade.cs
@@ -1,25 +1,26 @@
-﻿﻿using System.Collections; 
-using System.Collections.Generic; 
-using UnityEngine; 
-using HoloToolkit.Unity.InputModule; 
+﻿//﻿using System.Collections; 
+//using System.Collections.Generic; 
+//using UnityEngine;
 
-public class Rotater : MonoBehaviour, IInputClickHandler, IInputHandler  { 
-	public float smooth = 2.0F; 
-	public float tiltAngle = 30.0F; 
-	float tiltAroundY; 
-	void Update() { 
-		//float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle; 
-		tiltAroundY+= Time.deltaTime * smooth * tiltAngle; 
-		Quaternion target = Quaternion.Euler(0, tiltAroundY, 0); 
-		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth); 
-	} 
-	public void OnInputClicked(InputClickedEventData eventData) 
-	{ 
-		// AirTap code goes here 
-		//this.transform.Rotate(); 
-	} 
-	public void OnInputDown(InputEventData eventData) 
-	{ } 
-	public void OnInputUp(InputEventData eventData) 
-	{ } 
-} 
+//using HoloToolkit.Unity.InputModule; 
+
+//public class Rotater : MonoBehaviour, IInputClickHandler, IInputHandler  { 
+//	public float smooth = 2.0F; 
+//	public float tiltAngle = 30.0F; 
+//	float tiltAroundY; 
+//	void Update() { 
+//		//float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle; 
+//		tiltAroundY+= Time.deltaTime * smooth * tiltAngle; 
+//		Quaternion target = Quaternion.Euler(0, tiltAroundY, 0); 
+//		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth); 
+//	} 
+//	public void OnInputClicked(InputClickedEventData eventData) 
+//	{ 
+//		// AirTap code goes here 
+//		//this.transform.Rotate(); 
+//	} 
+//	public void OnInputDown(InputEventData eventData) 
+//	{ } 
+//	public void OnInputUp(InputEventData eventData) 
+//	{ } 
+//} 
