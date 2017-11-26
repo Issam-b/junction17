d928ca52d25d1d9a8919afc471e38b0ed2ce5d8f
MounMaz <maz.moun@yahoo.fr>
2017-11-26 07:11:53 +0200

    Adding Text to the Torque UI element

--- unity-planmeca/Assets/Scripts/ScenesManager.cs
+++ unity-planmeca/Assets/Scripts/ScenesManager.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ScenesManager : MonoBehaviour {
 
 	
    // enum State curentState ={ idle, drill};
 
 	void Start () {
-		
+		InvokeRepeating (ChangeScene (1), 60, 0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public void ChangeScene (int i) {
+		SceneManager.LoadScene (i);
 	}
 }
