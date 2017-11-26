8a8fa17a3d4084e710598de1d24d6908e681b32a
Issam <assam.bj@gmail.com>
2017-11-26 03:54:03 +0200

    clean the code

---
+++ Unity-trimble/Assets/script/AnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class AnimationController : MonoBehaviour
+{
+    public Transform target;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.DOMove(new Vector3(1.5f, 0, 0), 2);
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.DOMove(new Vector3(0, 0, 0), 2);
+
+        }
+
+    }
+}
