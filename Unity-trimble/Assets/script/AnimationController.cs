53e7765d59343ce7ad72ed7b8bdc7853de652c89
Issam <assam.bj@gmail.com>
2017-11-26 04:28:44 +0200

    add shift and zoom logic

--- Unity-trimble/Assets/script/AnimationController.cs
+++ Unity-trimble/Assets/script/AnimationController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using DG.Tweening;
 public class AnimationController : MonoBehaviour
 {
-    public Transform target;
+
+    public GameObject floor1;
+    Vector3 F1Initial, F2Initial;
+
     // Use this for initialization
     void Start()
     {
-        
+        F1Initial = floor1.transform.position;
+        F2Initial = transform.position;
+
     }
 
     // Update is called once per frame
@@ -16,14 +21,41 @@ public class AnimationController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.DOMove(new Vector3(1.5f, 0, 0), 2);
+            floor1.SetActive(true);
+            floor1.transform.position = F1Initial;
+            transform.position = F2Initial;
+            transform.DOScale(new Vector3(1, 1, 1), 1);
+            transform.DOMove(new Vector3(0, 0, 1.5f), 2);
             
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.DOMove(new Vector3(0, 0, 0), 2);
+            floor1.SetActive(true);
+            floor1.transform.position = F1Initial;
+            transform.position = F2Initial;
+            transform.DOScale(new Vector3(1, 1, 1), 1);
+            transform.DOMove(new Vector3(1.5f, 0, 1.5f), 2);
 
         }
 
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            ZoomFloor2();
+        }
+    //x 0.041894 y -0.568 z 0.75762  s 25
+
+}
+
+    public void OnSelect()
+    {
+        ZoomFloor2();
+    }
+
+    public void ZoomFloor2()
+    {
+            floor1.SetActive(false);
+            transform.DOMove(new Vector3(6f, -2f, 1.5f), 2);
+            transform.DOScale(new Vector3(25, 25, 25), 2);
+       
     }
 }
